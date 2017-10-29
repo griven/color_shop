c82cb5b18ad6f35190f47ccd21ad7ddd5300f104
griven <griveh@yandex.ru>
2017-10-29 20:10:46 +0300

    initial schemas

---
+++ db/migrations/20171029160808_create_initial_table.php
@@ -0,0 +1,47 @@
+<?php
+
+
+use Phinx\Migration\AbstractMigration;
+
+class CreateInitialTable extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change()
+    {
+        $this->table('box_volumes', ['id' => false, 'primary_key' => 'box_type'])
+            ->addColumn('box_type', 'char', ['length' => 2, 'comment' => 'тип тары'])
+            ->addColumn('volume', 'integer', ['comment' => 'объем (литр)'])
+            ->create();
+
+        $this->table('goods')
+            ->addColumn('shop_name', 'string', ['comment' => 'имя поставщика'])
+            ->addColumn('box_type', 'char', ['length' => 2, 'comment' => 'тип тары'])
+            ->addColumn('price', 'integer', ['comment' => 'цена товара'])
+            ->addForeignKey('box_type', 'box_volumes', 'box_type')
+            ->create();
+
+        $this->table('consumption')
+            ->addColumn('consumption', 'integer', ['comment' => 'расход (литр/метр кв.)'])
+            ->create();
+    }
+}
